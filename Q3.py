acf60ce09e3f39ee455cf4c11c247b5690c8ce46
Aashudeep <thakuraayush997@gmail.com>
2022-10-13 12:30:49 +0545

    my works

---
+++ Q3.py
@@ -0,0 +1,4 @@
+temp =float(input("Enter temperature in celcius:"))
+f =(temp*1.8)+32
+print(temp)
+print(f)
---
+++ Q4.py
@@ -0,0 +1,7 @@
+batted = 1014
+match = 609
+out = 162
+scored = 48426
+
+Avg = scored/(batted - out)
+print(Avg)
